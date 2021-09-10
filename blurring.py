b680c8d00c17c11423d7d657e237be089a4caa5e
James <james.hou.23@bishops.com>
2021-09-10 11:13:02 -0700

    chapter 7

---
+++ blurring.py
@@ -0,0 +1,12 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", '--image', required=True, help="Path to image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+blurring = np.stack
--- colorspaces.py
+++ colorspaces.py
@@ -11,3 +11,14 @@ cv2.imshow("Original", image)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", gray)
+
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV", hsv)
+
+lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+cv2.imshow("L*a*b*", lab)
+cv2.waitKey(0)
+
+print(gray.shape)
+print(hsv.shape)
+print(lab.shape)
